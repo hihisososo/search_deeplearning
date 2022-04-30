7aa2e0e6b7fe21801283e967d2e6abe8650b1e82
hihisososo <hihisososo@naver.com>
2022-04-30 19:04:44 +0900

    Chapter3 일반 검색에서 텍스트 생성까지

--- src/com/search/deeplearning/ch3/SampleLstm.java
+++ src/com/search/deeplearning/ch3/SampleLstm.java
@@ -1,5 +1,11 @@
 package com.search.deeplearning.ch3;
 
+import static com.search.deeplearning.utils.NeuralNetworksUtils.sampleFromDistribution;
+
+import com.search.deeplearning.utils.CharacterIterator;
+import com.search.deeplearning.utils.NeuralNetworksUtils;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import org.deeplearning4j.nn.conf.BackpropType;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
@@ -8,45 +14,58 @@ import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.io.ClassPathResource;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.Random;
-
 public class SampleLstm {
-    public static void main(String[] args) throws IOException {
-        int lstmLayerSize = 200;
-        int sequenceSize = 50;
-        int unrollSize = 10;
-        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .list()
-                .layer(0, new LSTM.Builder()
-                        .nIn(sequenceSize)
-                        .nOut(lstmLayerSize)
-                        .activation(Activation.TANH).build())
-                .layer(1, new RnnOutputLayer.Builder(LossFunctions.LossFunction.MCXENT)
-                        .activation(Activation.SOFTMAX)
-                        .nIn(lstmLayerSize)
-                        .nOut(sequenceSize).build())
-                .backpropType(BackpropType.TruncatedBPTT)
-                .tBPTTForwardLength(unrollSize)
-                .tBPTTBackwardLength(unrollSize)
-                .build();
 
-        MultiLayerNetwork net = new MultiLayerNetwork(conf);
-        net.init();
-        net.setListeners(new ScoreIterationListener(1));
+  public static void main(String[] args) throws IOException {
+    int lstmLayerSize = 200;
+    int sequenceSize = 77;
+    int unrollSize = 10;
+    MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+        .list()
+        .layer(0, new LSTM.Builder()
+            .nIn(sequenceSize)
+            .nOut(lstmLayerSize)
+            .activation(Activation.TANH).build())
+        .layer(1, new LSTM.Builder()
+            .nIn(lstmLayerSize)
+            .nOut(lstmLayerSize)
+            .activation(Activation.TANH).build())
+        .layer(2, new RnnOutputLayer.Builder(LossFunctions.LossFunction.MCXENT)
+            .activation(Activation.SOFTMAX)
+            .nIn(lstmLayerSize)
+            .nOut(sequenceSize).build())
+        .backpropType(BackpropType.TruncatedBPTT)
+        .tBPTTForwardLength(unrollSize)
+        .tBPTTBackwardLength(unrollSize)
+        .build();
 
-        char[] alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX".toCharArray();
-        String path = new ClassPathResource("shakesphere.txt")
-                .getFile()
-                .getAbsolutePath();
-        CharacterIterator iterator = new CharacterIterator(path, Charset.forName("UTF-8"), 10, 10, alphabet, new Random());
-        while (iterator.hasNext()) {
-            net.fit(iterator);
-        }
-
+    String path = new ClassPathResource("shakesphere.txt").getFile().getAbsolutePath();
+    CharacterIterator iterator = new CharacterIterator(path, Charset.forName("UTF-8"), 50, 50);
+    MultiLayerNetwork net = new MultiLayerNetwork(conf);
+    net.init();
+    net.setListeners(new ScoreIterationListener(1));
+    while (iterator.hasNext()) {
+      net.fit(iterator);
     }
+
+    StringBuilder initialization = new StringBuilder();
+    initialization.append("Look in thy glass and");
+
+      INDArray input = Nd4j.zeros(sequenceSize, initialization.length());
+      char[] init = initialization.toString().toCharArray();
+      for (int i = 0; i < init.length; i++) {
+        int idx = iterator.convertCharacterToIndex(init[i]);
+
+        input.putScalar(new int[]{idx, 1}, 1.0f);
+      }
+      INDArray output = net.rnnTimeStep(input);
+      int sampledCharacterIdx = sampleFromDistribution(output.toDoubleVector());
+      char c = iterator.convertIndexToCharacter(sampledCharacterIdx);
+      System.out.print(c);
+  }
 }
